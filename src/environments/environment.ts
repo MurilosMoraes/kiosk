4892e6d6720b06b3cd90577d6b48c066a080566d
Murilo Moraes <58737273+MurilosMoraes@users.noreply.github.com>
2025-07-01 13:49:47 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase:{
-    apiKey: "AIzaSyAuQU_MRLbNPixpVBv6PlbIG-2qUvw1GGQ",
-    authDomain: "kiosk-web-app.firebaseapp.com",
-    projectId: "kiosk-web-app",
-    storageBucket: "kiosk-web-app.appspot.com",
-    messagingSenderId: "747092675169",
-    appId: "1:747092675169:web:18f84c9911f1156dfe3820"
-  }
+
 };
 
 
